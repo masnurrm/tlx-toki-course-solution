689cf3fdd37c8b794ee0fee982f1927ad51ef42e
Nur Muhammad 'Ainul Yaqin <sangmastahnm@gmail.com>
2020-10-27 21:12:26 +0700

    Create EkspresiMasukan-TranposeMatriks.cpp

---
+++ EkspresiMasukan-TranposeMatriks.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c,d,e,f,g,h,i;
+	cin>>a>>b>>c>>d>>e>>f>>g>>h>>i;
+	cout<<a<<" "<<d<<" "<<g<<"\n"<<b<<" "<<e<<" "<<h<<"\n"<<c<<" "<<f<<" "<<i<<endl;
+	return 0;
+}
